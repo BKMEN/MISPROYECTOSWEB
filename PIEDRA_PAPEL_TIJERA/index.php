29098e33ebcb9bc4cb8d5470845c4786ea75a08c
BKMEN <yovanjcd@gmail.com>
2021-11-30 14:12:40 +0100

    PIEDRA PAPEL TIJERA v.2

--- PIEDRA_PAPEL_TIJERA/index.php
+++ PIEDRA_PAPEL_TIJERA/index.php
@@ -3,13 +3,9 @@
 session_start();
 if (! isset($_POST['seleccionar']) ){ //si no envio nada
   $_SESSION['seleccionar']=rand(0,2);
-  $_SESSION['intentos']=0;
 }
 $maquina = $_SESSION['seleccionar'];
-$intentos=$_SESSION['intentos'];
-$_SESSION['intentos']=$_SESSION['intentos']+1;
-echo "llevas"." ".$intentos." intentos";
-$_POST['seleccionar']=$jugador;
+$jugador=$_POST['seleccionar'];
 ?>
 
 
@@ -23,7 +19,7 @@ $_POST['seleccionar']=$jugador;
   </head>
   <body>
   <!--CUADRO AZUL-->
-  <div class="formulario centrado">
+  <div class="formulario centrado desaparecer">
     <form method="post">
 <img class="imagenes-seleccionar" src="0.jpg" alt="">
 <input class="seleccionar" type="radio" name="seleccionar" value="0">
@@ -37,31 +33,30 @@ $_POST['seleccionar']=$jugador;
   </form>
   </div>
   <div class="resultado centrado">
-    <?php if (isset($_POST['seleccionar']) ) {
+    <?php if (isset($_POST['seleccionar'])
+  ) {
            if ( $maquina == $_POST['seleccionar'] ) {
-             echo "<h1>EMPATE</h1>";
+             echo '<h1 class="tie">EMPATE</h1><br>';
            }
-         } ?>
-         <?php
+
+
    if( isset($_POST['seleccionar']) ){
-     echo "<h3>Has enviado algo :" ." ". $_POST['seleccionar']."</h3>";
+
 
      if(($_POST['seleccionar']==0&&$maquina==2)||($_POST['seleccionar']==1&&$maquina==0)||($_POST['seleccionar']==2&&$maquina==1))
-         echo 'HAS GANADO<br>';
+         echo '<h1 class="won">HAS GANADO</h1><br>';
      if(($_POST['seleccionar']==2&&$maquina==0)||($_POST['seleccionar']==0&&$maquina==1)||($_POST['seleccionar']==1&&$maquina==2))
-         echo 'HAS PERDIDO<br>';
+         echo '<h1 class="lost">HAS PERDIDO</h1><br>';
        }
+
       ?>
-
-
-
-
   <?php
 
   echo "<h2>la maquina ha sacado</h2>";
 
   {?>
     <img src='<?=$maquina?>.jpg' WIDTH='70'HEIGHT='55'>
+  <link rel="stylesheet" href="style2.css">
   <?php } ?>
 
   <?php
@@ -70,7 +65,7 @@ $_POST['seleccionar']=$jugador;
 
   {?>
     <img src='<?=$jugador?>.jpg' WIDTH='70'HEIGHT='55'>
-  <?php } ?>
+  <?php } }  ?>
   <br>
   <p>  <a href="index.php">Reiniciar partida</a></p>
   </div>
